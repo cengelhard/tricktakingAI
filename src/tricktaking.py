3c589c31f352f6b377394b0298896b8619309cfc
CHarlie <ochuckles@gmail.com>
2020-01-28 17:41:25 -0800

    small refactor

--- src/tricktaking.py
+++ src/tricktaking.py
@@ -7,6 +7,7 @@ from pyrsistent import (PRecord, field,
 						l, plist)
 from deck import deck52, Card
 import random as rand
+import sys
 
 def hearts_points(card):
 	return 1 if card.suit == "♥" else 13 if card.key == "Q♠" else 0
@@ -28,35 +29,49 @@ class HeartsPlayer(PRecord):
 
 	penalty = field(int)
 
+
+	def points_sans_penalty(self):
+		return sum(map(hearts_points, self.won))
 	def points(self):
-		return sum(map(hearts_points, self.won)) + self.penalty
+		return self.points_sans_penalty() + self.penalty
 	def total_points(self):
 		return self.points() + (self.prev_player.total_points() if self.prev_player else 0)
 	def privatize(self):
 		return self.set(hand=s())
 	def play_card(self, card):
 		return self.set(played = self.played.append(card), hand=self.hand.remove(card))
-	def with_penalty(self):
-		return self.set(penalty=self.penalty+1)
+	def with_penalty(self, amount=1):
+		return self.set(penalty=self.penalty+amount)
+	def shot_the_moon(self):
+		return self.points_sans_penalty() == 26
 
+class PlayerVec(CheckedPVector):
+	__type__ = HeartsPlayer
 
 #the four players
-class GameState(CheckedPVector):
-	__type__ = HeartsPlayer
-	#__invariant__ = len4
+class GameState(PRecord):
 
-	def private_to(self, i): #i should not be able to see others' hands
-		return GameState.create([self[j] if j==i else self[j].privatize() for j in range(4)])
+	players = field(PlayerVec, invariant=len4)
+	trick_leader = field(int, invariant=lambda x: (x<4, f"trick_leader cannot be {x}"))
+
+	def set_player(self, pid, player):
+		return self.set(players=self.players.set(pid, player))
+	def map_players(self, f):
+		return self.set(players=PlayerVec.create(map(f, range(4), self.players)))
+	def private_to(self, pid): #i should not be able to see others' hands
+		return self.map_players(lambda i, p: p if pid==i else p.privatize())
 	#returns both positional and list form.
 	def played_this_trick(self):
-		lens = [len(p.played) for p in self]
+		lens = [len(p.played) for p in self.players]
 		m = max(lens)
-		positional = pvector([p.played[-1] if len(p.played)==m else None for p in self])
+		positional = pvector([p.played[-1] if len(p.played)==m else None for p in self.players])
 		return positional, pvector(filter(None, positional))
+	#returns none if there are 4 cards played... ambiguous as far as it knows.
 	def hand_done(self):
-		return all(len(p.played)==13 for p in self)
+		return all(len(p.played)==13 for p in self.players)
 	def game_done(self):
-		return any(p.total_points() > 50 for p in self)
+		return any(p.total_points() > 50 for p in self.players)
+
 
 def deal_4_players(deck):
 	return [pset(h) for h in [deck[:13], deck[13:26], deck[26:39], deck[39:]]]
@@ -67,7 +82,6 @@ def deal_4_players(deck):
 #each player passes a callback to their function which is passed their hand and returns their choices.
 #those returns get combined together into the continuation function.
 #that returns a new continuation
-
 def HeartsGame():
 
 	def nextp(i):
@@ -75,9 +89,15 @@ def HeartsGame():
 
 	#takes a previous state from previous hands.
 	def play_hand(prior_state):
-		deck = deck52.copy()
-		rand.shuffle(deck)
-		hands = deal_4_players(deck)
+
+		hands = None
+		if len(prior_state.players[0].hand):
+			#it is possible that the prior state already has hands set.
+			hands = [p.hand for p in prior_state]
+		else:
+			deck = deck52.copy()
+			rand.shuffle(deck)
+			hands = deal_4_players(deck)
 
 		def pass_left(chooser_fns):
 			
@@ -85,12 +105,10 @@ def HeartsGame():
 			bad_choices = [len(cs) != 3 for cs in choices]
 			if any(bad_choices):
 				#try again with penalties.
-				return pass_left(GameState.create([prior_state[i] if not bad_choices[i] else prior_state[i].with_penalty() for i in range(4)]))
+				return pass_left(prior_state.map_players(lambda i, p: p if not bad_choices[i] else p.with_penalty()))
 
 			new_hands = [hands[i].difference(choices[i]).union(choices[nextp(i)]) for i in range(4)]
-			state = GameState.create([prior_state[i].set(hand=new_hands[i],
-														 played = v(), 
-														 prev_player=prior_state[i]) for i in range(4)])
+			state = prior_state.map_players(lambda i, p: p.set(hand=new_hands[i], played=v(), prev_player=p))
 			
 			#imperative programming outa nowhere!
 			leader_i = 0
@@ -98,7 +116,7 @@ def HeartsGame():
 			two_of_clubs = None 
 
 			for i in range(4):
-				p = state[i]
+				p = state.players[i]
 				h = p.hand
 				for c in h:
 					if c.key == "2♣":
@@ -108,15 +126,15 @@ def HeartsGame():
 						break
 				if two_of_clubs:
 					break
-			new_state = state.set(leader_i, leader_p.play_card(two_of_clubs))
-			return play_trick_card(nextp(leader_i), new_state)
+			new_state = state.set_player(leader_i, leader_p.play_card(two_of_clubs))
+			return play_trick_card(nextp(leader_i), new_state.set(trick_leader=leader_i))
 
 		return "pass left", pass_left
 
 	def play_trick_card(pid, state):
-		player = state[pid]
+		player = state.players[pid]
 		hand = player.hand
-		played, only_played = state.played_this_trick() #now indexed by pid
+		played, only_played = state.played_this_trick()
 		def cont(chooser_fns):
 			card = chooser_fns[pid](hand, state.private_to(pid))
 			valid_card = (len(only_played) == 4)
@@ -127,9 +145,9 @@ def HeartsGame():
 				valid_card &= (card in hand)
 				if not valid_card:
 					#try again with penalty
-					return play_trick_card(pid, state.set(pid, player.with_penalty()))
+					return play_trick_card(pid, state.set_player(pid, player.with_penalty()))
 			
-			played_state = state.set(pid, player.play_card(card))
+			played_state = state.set_player(pid, player.play_card(card))
 
 			new_played, new_only_played = played_state.played_this_trick()
 
@@ -146,22 +164,27 @@ def HeartsGame():
 					if card2.suit == lsuit and card2.rank >= best_rank:
 						best_pid = i
 						best_rank = card2.rank
-						winner = played_state[i]
-				winner_state = played_state.set(best_pid, winner.set(won = winner.won.extend(new_played)))
+						winner = played_state.players[i]
+				winner_state = played_state.set_player(best_pid, winner.set(won = winner.won.extend(new_played)))
 				if winner_state.hand_done():
 					if winner_state.game_done():
 						return "game over", winner_state
 					else:
-						return play_hand(winner_state)
+						moon_shooters = pvector(p.shot_the_moon() for p in winner_state.players)
+						if (any(moon_shooters)):
+							moon_shooter = moon_shooters.index(True)
+							return play_hand(winner_state.map_players(lambda i, p: p.with_penalty(-26 if i==moon_shooter else 26)))
+						else:
+							return play_hand(winner_state)
 				else:
 					return play_trick_card(best_pid, winner_state)	
 		return "play a card", cont
 
-	initial_state = GameState.create([HeartsPlayer(played=v(),
+	initial_state = GameState(players=PlayerVec.create([HeartsPlayer(played=v(),
 												    won = v(),
 												    prev_player = None,
 												    penalty = 0,
-												    hand=s()) for _ in range(4)])
+												    hand=s()) for _ in range(4)]))
 	return play_hand(initial_state)
 
 
@@ -169,7 +192,11 @@ def HeartsGame():
 def ControlledGame(game, controllers):
 	msg, cont = game()
 	while msg != "game over":
-		msg, cont = cont([c[msg] for c in controllers])
+		try:
+			msg, cont = cont([c[msg] for c in controllers])
+		except QuitGameException:
+			print("quitting game")
+			return
 	return cont
 
 #a completely random AI.
@@ -195,27 +222,37 @@ def stupid_test():
 	game_state = ControlledGame(HeartsGame, [stupid_controller(i) for i in range(4)])
 	print([p.total_points() for p in game_state])
 
-def input_controller(pid):
+class QuitGameException(Exception):
+	pass
 
-	def slash_commands(str):
-		if str[0] == '/':
-			command = str[1:]
-			if command == "quit":
-				pass
-			return True
+def input_controller(pid):
 
 	def print_hand(hand):
 		print("your hand:")
 		print("|".join(f"{i}:{card.key}" for i,card in enumerate(hand)))
+
+	def _input(hand):
+
+		string = input("--->")
+		if string[0] == '/':
+			command = string[1:]
+			if command == "quit":
+				raise QuitGameException()
+			elif command == "hand":
+				print_hand(hand)
+				return _input(hand)
+		return string
 
 	def pass_left(hand):
 		print(f"player {pid}, please choose 3 card indecies, separated by commas.")
 		hand = list(hand)
 		print_hand(hand)
 		try:
-			choices = [hand[int(sub)] for sub in input("--->").split(',')]
+			choices = [hand[int(sub)] for sub in _input(hand).split(',')]
 			if len(choices)==3:
 				return choices
+		except QuitGameException:
+			raise QuitGameException()
 		except:
 			pass
 		print("something wasn't right.")
@@ -239,15 +276,17 @@ def input_controller(pid):
 		hand = list(hand)
 		print_hand(hand)
 		try:
-			choice = hand[int(input("--->"))]
+			choice = hand[int(_input(hand))]
 			return choice
+		except QuitGameException:
+			raise QuitGameException()
 		except:
 			pass
 		print("something wasn't right")
 		return play_trick(hand, state)
 
 	#doesn't ask anything, just informing you for possible IO
-	#(doesn't even give you information that a futre msg won't.)
+	#(doesn't even give you information that a future msg won't.)
 	def someone_else(other_pid, card, state):
 		print(f"{other_pid} played {card.key}")
 
@@ -257,6 +296,6 @@ def input_controller(pid):
 
 def play_with_stupid():
 	game_state = ControlledGame(HeartsGame, [stupid_controller(i) if i>0 else input_controller(i) for i in range(4)])
-	print([p.total_points() for p in game_state])
+	print([p.total_points() for p in game_state.players] if game_state else "game quit early")
 
 
