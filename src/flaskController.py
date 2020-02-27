d33d088f4a44692b2189262453f99fff7be2c0cc
CHarlie <ochuckles@gmail.com>
2020-02-27 15:18:58 -0800

    use port 80

--- src/flaskController.py
+++ src/flaskController.py
@@ -231,7 +231,7 @@ def player_info(gid, pid):
         })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8001, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
 
 '''
 TODO:
